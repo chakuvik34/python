db44a671504b885404050f5e2c4cf2f10facf46b
chakuvik34 <chakuvik34@gmail.com>
2023-08-16 10:46:42 +0530

    Done

---
+++ operateroverload.py
@@ -0,0 +1,24 @@
+class point:
+    def __init__(self,x,y):
+        print("init called")
+        self.x=x
+        self.y=y
+    def __str__(self):
+        print("str called")
+        return "{0},{1}".format(self.x,self.y)
+    def __add__(self,obj):
+        print("add called")
+        x=self.x+obj.x
+        y=self.y+obj.y
+        return point(x,y)
+    def __sub__(self,obj):
+        print("sub called")
+        x=self.x-obj.x
+        y=self.y-obj.y
+        return point(x,y)
+p1=point(10,20)
+print(p1)
+p2=point(30,40)
+print(p2)
+print("Addition of objacts : ",p1+p2)
+print("subtraction of objacts : ",p1-p2)    
