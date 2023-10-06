0c7fb4bffd96c14b8875d601bb9dd56d453b7788
chakuvik34 <140376766+chakuvik34@users.noreply.github.com>
2023-10-06 17:49:26 +0530

    Delete T21.py

--- T21.py
+++
@@ -1,8 +0,0 @@
-
-input_string = input("Enter a string: ")
-
-if len(input_string) < 2:
-    result_string = ""
-else:
-    result_string = input_string[:2] + input_string[-2:]
-print("Result:", result_string)
