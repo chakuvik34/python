a9c27e51ade3addb4f2dc7a4cca0f0401da4c04b
chakuvik34 <140376766+chakuvik34@users.noreply.github.com>
2023-10-06 17:40:56 +0530

    Delete T16.py

--- T16.py
+++
@@ -1,9 +0,0 @@
-s1 = input("Enter the first string: ")
-s2= input("Enter the second string: ")
-
-swapped_s1 = s2[:2] + s1[2:]
-swapped_s2 = s1[:2] + s2[2:]
-
-ans = swapped_s1 + ' ' + swapped_s2
-
-print(f"The result string is: {ans}")
